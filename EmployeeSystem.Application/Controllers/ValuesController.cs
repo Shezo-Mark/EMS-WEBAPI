0fa1b7f4741d1074d0edfa591eee0f10b62647c1
bilalrafique0900 <bilal.rana0900@gmail.com>
2025-11-04 12:18:35 +0500

    save

---
+++ EmployeeSystem.Application/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeSystem.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
